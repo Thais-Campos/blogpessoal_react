f54f0cbd963495bc9aff46c566b801865b43bd92
Thaís <tscampos31@gmail.com>
2026-01-14 16:13:57 -0300

    cookbook 6 ao 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
+    <>
+     <div className="min-h-screen flex flex-col">
+    <Navbar />
+     <main className="flex-1 bg-indigo-900">
     <Home />
+    </main>
+    <Footer />
+    </div>
+    </>
   );
 }
 
